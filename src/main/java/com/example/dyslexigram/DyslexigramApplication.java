ed2c8e826eb92ea3b766ccd14bacf473568b9ddf
Damjan Denkovski <damjandenkovski77@gmail.com>
2023-02-26 20:12:53 +0100

    Initial project

---
+++ src/main/java/com/example/dyslexigram/DyslexigramApplication.java
@@ -0,0 +1,13 @@
+package com.example.dyslexigram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyslexigramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DyslexigramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dyslexigram/DyslexigramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dyslexigram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyslexigramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
