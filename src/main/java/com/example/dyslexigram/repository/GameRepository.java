e1f850f11bc69c3436871ceeb068b5e5886dd0d3
Zagorka <jovanovazagorka@outlook.com>
2023-07-14 18:51:49 +0200

    Enable answering questions

--- src/main/java/com/example/dyslexigram/repository/GameRepository.java
+++ src/main/java/com/example/dyslexigram/repository/GameRepository.java
@@ -1,8 +1,11 @@
 package com.example.dyslexigram.repository;
 
 import com.example.dyslexigram.model.Game;
+import com.example.dyslexigram.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game,Long> {
